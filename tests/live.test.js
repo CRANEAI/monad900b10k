50105990c190d03cef454782f976efd9c938daa8
Elly <elly@mg.crane.ai>
2018-11-05 15:26:26 -0500

    added tests/live refactoring

---
+++ tests/live.test.js
@@ -0,0 +1,21 @@
+/* 
+ * Contact: team@fusioncode.ai
+ *
+ *
+ * Project  - live UNIT TESTS
+ * Application Client
+ * MonDos
+*/
+
+/* the payment view has a back icon located at the very-top left of the view which on click will trigger the back() action */
+it("the payment view has a back icon located at the very-top left of the view which on click will trigger the back() action", () => {
+  /* ToDo developer needs to complete unit test 
+               the payment view has a back icon located at the very-top left of the view which on click will trigger the back() action
+               uncomment below and update the code to test
+                //instance.action(function(error) {
+                //  if (error) throw error;
+                //expect().to.be();
+                //});
+                done();
+               */
+})
