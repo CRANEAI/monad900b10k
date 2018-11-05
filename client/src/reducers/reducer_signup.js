691e9e52207c9297df180ca1e1b7d2fccaf84143
Elly <elly@mg.crane.ai>
2018-11-05 15:24:51 -0500

    updated client/src/reducers/reducer_signup refactoring

--- client/src/reducers/reducer_signup.js
+++ client/src/reducers/reducer_signup.js
@@ -3,8 +3,19 @@ var DEFAULT_STATE = {
   errorMessage: null
 }
 
+const postSignup = (state, action) => {
+  let newState = {}
+  let data = action.payload
+
+  Object.assign(newState, state, { data })
+  return newState
+}
+
 export default function(state = DEFAULT_STATE, action) {
   switch (action.type) {
+    case "POSTSIGNUP":
+      return postSignup(state, action)
+
     default:
       return state
   }
