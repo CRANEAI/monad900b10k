8dd64bb7d24da742f11e5ddb80fb7fc140cdc6cc
Elly <elly@mg.crane.ai>
2018-11-05 15:25:39 -0500

    added tests/create_account refactoring

---
+++ tests/create_account.test.js
@@ -0,0 +1,21 @@
+/* 
+ * Contact: team@fusioncode.ai
+ *
+ *
+ * Project  - create account UNIT TESTS
+ * Application Client
+ * MonDos
+*/
+
+/* the create account view has a back icon located at the very-top left of the view which on click will trigger the back() action */
+it("the create account view has a back icon located at the very-top left of the view which on click will trigger the back() action", () => {
+  /* ToDo developer needs to complete unit test 
+               the create account view has a back icon located at the very-top left of the view which on click will trigger the back() action
+               uncomment below and update the code to test
+                //instance.action(function(error) {
+                //  if (error) throw error;
+                //expect().to.be();
+                //});
+                done();
+               */
+})
